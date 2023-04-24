abb28df10443df8ff2391cfc041b320a1b35cb99
Lebedev Vitaly <11132166@mail.ru>
2023-04-24 18:53:53 +0300

    Выполнил 4  задание

---
+++ task4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число:");
+int A = int.Parse(Console.ReadLine()!);
+int B = 0;
+while (A != B)
+{
+    if ( B % 2 ==0 )
+    {
+       Console.Write(B + " ");
+       B++;
+    }
+    else
+    {
+        B++;
+    }
+}
