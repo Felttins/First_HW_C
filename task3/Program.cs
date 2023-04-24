b1c89dbaa692dbc23a284330061314392d5c18e2
Lebedev Vitaly <11132166@mail.ru>
2023-04-24 18:29:54 +0300

    Выполнил 3  задание

---
+++ task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine()!);
+if (A % 2 == 1)
+{
+    Console.WriteLine($"Число {A}, - не чётное");
+}
+else
+{
+    Console.WriteLine($"Число {A}, - чётное");
+}
