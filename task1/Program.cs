c854bed0a53ce767752061d747b17cba29954eaf
Lebedev Vitaly <11132166@mail.ru>
2023-04-24 17:44:42 +0300

    Сделал нахождение максимального числа

---
+++ task1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите число 1: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число 2: ");
+int B = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число 3: ");
+int C = int.Parse(Console.ReadLine()!);
+if (A > B)
+{
+    if (A > C)
+    {
+        Console.WriteLine(A);
+    }
+}
+else
+    {
+        if (B > C)
+        {
+            Console.WriteLine(B);
+        }
+        else
+        {
+            Console.WriteLine(C);
+        }
+    }
