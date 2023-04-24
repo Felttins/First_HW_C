8b912a66a85abe0aa52243264be654ce1e94a969
Lebedev Vitaly <11132166@mail.ru>
2023-04-24 18:10:12 +0300

    Выполнил 2 задание

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.Write("Введите число 1:");
+int A = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число 2:");
+int B = int.Parse(Console.ReadLine()!);
+if (A > B)
+{
+    Console.WriteLine($"Максимальное число: {A}, Минимальное число: {B}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число: {B}, Минимальное число: {A}");
+}
